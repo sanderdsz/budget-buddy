9b99ef3426b8569a6eeaeea2a1ddd46f7bdace47
sander <sanderdsz@gmail.com>
2023-08-31 20:08:46 -0300

    chore: CSP policy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const { NEXT_PUBLIC_API_BASE_URL } = process.env;
+
 const nextConfig = {
   reactStrictMode: true,
+  headers: [
+    {
+      key: 'Content-Security-Policy',
+      value: `default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self'; img-src 'self'; connect-src 'self' ${NEXT_PUBLIC_API_BASE_URL};`,
+    }
+  ]
 }
 
 module.exports = nextConfig
