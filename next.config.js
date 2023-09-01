458eaee0f84422eab79dba19de5b72f93f18e204
sanderdsz <sanderdsz@gmail.com>
2023-08-31 21:03:22 -0300

    fix: CSP

--- next.config.js
+++ next.config.js
@@ -3,13 +3,21 @@ const { NEXT_PUBLIC_API_BASE_URL } = process.env;
 
 const nextConfig = {
   reactStrictMode: true,
-  source: '/(.*)',
-  headers: [
-    {
-      key: 'Content-Security-Policy',
-      value: "default-src 'self' http://168.75.75.99:8080",
-    },
-  ],
 }
-
 module.exports = nextConfig
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self' http://168.75.75.99:8080",
+          },
+        ],
+      }
+    ]
+  }
+}
