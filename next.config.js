a84593d4f714b1cb66af6ce3ac1f6dfc3f769c4c
sander <sanderdsz@gmail.com>
2023-09-01 09:15:26 -0300

    fix: revert CSP

--- next.config.js
+++ next.config.js
@@ -1,33 +1,5 @@
 /** @type {import('next').NextConfig} */
-const { NEXT_PUBLIC_API_BASE_URL } = process.env;
-
 const nextConfig = {
   reactStrictMode: true,
 }
-module.exports = nextConfig
-
-const ContentSecurityPolicy = `
-  default-src 'self' 'unsafe-inline' http://168.75.75.99:8080;
-  script-src 'self' 'unsafe-inline';
-  child-src example.com;
-  style-src 'self' example.com fonts.googleapis.com;
-  style-src-elem 'self' example.com;
-  font-src 'self';
-  connect-src http://168.75.75.99:8080;
-`;
-
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-          },
-        ],
-      }
-    ]
-  }
-}
+module.exports = nextConfig;
